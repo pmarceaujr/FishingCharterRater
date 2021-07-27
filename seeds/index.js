ed5663ee25155a113045b3f06c84fdffafa5ebad
Paul Marceau <paul@themarceaus.net>
2021-07-27 16:15:05 -0400

    Updated model layout

--- seeds/index.js
+++ seeds/index.js
@@ -23,10 +23,13 @@ const seedDB = async () => {
     await Charter.deleteMany({});
     for (let i = 0; i < 100; i++) {
         const random37 = Math.floor(Math.random() * 37)
+        const price = Math.floor(Math.random() * 600 + 25)
         const charter = new Charter({
             location: `${cities[random37].city}, ${cities[random37].state}`,
-            name: `${sample(descriptors)} ${sample(places)}`
-
+            name: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae sit quaerat amet corrupti saepe fugiat cum ab laboriosam rem repudiandae consequuntur quibusdam praesentium dolorum, provident eaque doloremque quos officia tenetur.',
+            price: price
         })
         console.log(`${sample(descriptors)} ${sample(places)}`)
         await charter.save();
